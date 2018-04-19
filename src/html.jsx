38f1017aaca93448ff4ad98e0eb0e62d6eeaa613
luubinhan <an.luu@idealogic.com.vn>
2018-04-19 15:47:12 +0700

    no message

--- src/html.jsx
+++ src/html.jsx
@@ -37,7 +37,7 @@ export default class HTML extends React.Component {
           {this.props.headComponents}
           <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" />
           <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Noto+Sans:400,700&amp;subset=vietnamese" />
-          <link rel="stylesheet" type="text/css" href="https://luubinhan.github.io/sunshine/static/ionicons.min.css" />
+          <link rel="stylesheet" type="text/css" href="https://luubinhan.github.io/sunshine/css/ionicons.min.css" />
           <link rel="shortcut icon" href={favicon} />
           {css}
         </head>
