4aa4965e67f03897c3eb33e4eecb5d18cee4c686
luubinhan1989@gmail.com <luubinhan1989@gmail.com>
2018-04-28 15:54:02 +0700

    +

--- src/html.jsx
+++ src/html.jsx
@@ -42,6 +42,9 @@ export default class HTML extends React.Component {
           {css}
         </head>
         <body>
+          <div className="zalo-chat-widget" data-oaid="271604692846173383" data-welcome-message="Rất vui khi được hỗ trợ bạn!" data-autopopup="0" data-width="350" data-height="420"></div>
+
+          <script src="https://sp.zalo.me/plugins/sdk.js"></script>
           <div
             id="___gatsby"
             dangerouslySetInnerHTML={{ __html: this.props.body }}
