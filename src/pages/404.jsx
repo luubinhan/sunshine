b897c9f60d5b06a9ae971b5bf8cff5bc93a6adb8
luubinhan <an.luu@idealogic.com.vn>
2018-05-03 16:31:18 +0700

    +

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import Link from 'gatsby-link'
 
+import config from '../../data/SiteConfig';
+
 class NotFound extends Component {
   render() {
     return (
@@ -8,9 +10,9 @@ class NotFound extends Component {
         <div className="inner">
           <h1>404. Whoops!</h1>
           <p>Internet quá rộng lớn, đừng để lạc mất nhau!</p>
-          <Link href="/" className="btn btn-light">
+          <a href={config.fullURL} className="btn btn-light">
             <i className="ion-arrow-left-c"/> Quay lại trang chủ
-          </Link>
+          </a>
         </div>
       </div>
     )
