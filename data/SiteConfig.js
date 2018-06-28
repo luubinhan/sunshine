804d3825ffa5472b89705643ab60d51fc731f2a7
luubinhan <an.luu@idealogic.com.vn>
2018-06-28 16:40:59 +0700

    +

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -3,11 +3,11 @@ module.exports = {
   siteTitle: 'Shop chuyên cung cấp quần áo trẻ em - Mặt Trời Nhỏ', // Site title.
   siteTitleAlt: 'Shop chuyên cung cấp quần áo trẻ em - Mặt Trời Nhỏ', // Alternative site title for SEO.
   siteLogo: '/logos/logo-1024.png', // Logo used for SEO and manifest.
-  siteUrl: 'https://luubinhan.github.io', // Domain of your website without pathPrefix.
-  pathPrefix: '/sunshine', // Prefixes all links. For cases when deployed to example.github.io/sunshine/.
+  siteUrl: 'http://shopmattroinho.com', // Domain of your website without pathPrefix.
+  pathPrefix: '/', // Prefixes all links. For cases when deployed to example.github.io/sunshine/.
   siteDescription: 'Shop chuyên cung cấp quần áo trẻ em - Mặt Trời Nhỏ', // Website description used for RSS feeds/meta description tag.
   siteRss: '/rss.xml', // Path to the RSS file.
-  fullURL: 'https://luubinhan.github.io/sunshine',
+  fullURL: 'http://shopmattroinho.com',
   siteFBAppID: '', // FB Application ID for using app insights
   googleAnalyticsID: '', // GA tracking ID.
   disqusShortname: '', // Disqus shortname.
