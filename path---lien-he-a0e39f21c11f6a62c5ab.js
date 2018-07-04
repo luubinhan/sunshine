9d1fdc92a70b87651a7f8af78d3e8c4356402f90
An Luu <an.luu@idealogic.com.vn>
2018-07-04 13:10:52 +0700

    Updates

--- path---lien-he-a0e39f21c11f6a62c5ab.js
+++ path---lien-he-a0e39f21c11f6a62c5ab.js
@@ -1,2 +1,2 @@
-webpackJsonp([0x8057fa9d90ec],{998:function(t,e){t.exports={pathContext:{}}}});
+webpackJsonp([0x8057fa9d90ec],{997:function(t,e){t.exports={pathContext:{}}}});
 //# sourceMappingURL=path---lien-he-a0e39f21c11f6a62c5ab.js.map
--- path---size-chuan-kich-thuoc-quan-ao-tre-em-a0e39f21c11f6a62c5ab.js
+++ path---size-chuan-kich-thuoc-quan-ao-tre-em-a0e39f21c11f6a62c5ab.js
@@ -1,2 +1,2 @@
-webpackJsonp([25962747476051],{1e3:function(t,n){t.exports={pathContext:{}}}});
+webpackJsonp([25962747476051],{999:function(t,n){t.exports={pathContext:{}}}});
 //# sourceMappingURL=path---size-chuan-kich-thuoc-quan-ao-tre-em-a0e39f21c11f6a62c5ab.js.map
